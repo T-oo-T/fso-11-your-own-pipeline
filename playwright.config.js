0a7ae888b601e81f6b433cf4dbb18a1eef7b1540
Otto Peräkylä <otto.perakyla@gmail.com>
2025-08-04 12:38:52 +0300

    build before running e2e tests

--- playwright.config.js
+++ playwright.config.js
@@ -72,7 +72,7 @@ export default defineConfig({
   ],
   /* Run your local dev server before starting the tests */
   webServer: {
-     command: 'npm run start-prod',
+     command: 'npm run start-e2e',
      url: 'http://localhost:5000',
      timeout: 120 * 1000,
      reuseExistingServer: !process.env.CI,
